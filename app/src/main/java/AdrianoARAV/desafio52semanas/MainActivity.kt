24a3c9fe19ea6a850efe6f1d92ddd6dcbe998d2c
adriano <adrianoskte@gmail.com>
2020-12-16 20:46:47 -0300

    feat(Home):cria layout

--- app/src/main/java/AdrianoARAV/desafio52semanas/MainActivity.kt
+++ app/src/main/java/AdrianoARAV/desafio52semanas/MainActivity.kt
@@ -1,11 +1,17 @@
 package AdrianoARAV.desafio52semanas
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnNoTargetsButton.setOnClickListener {
+            Toast.makeText(this, "Iniciando Toast!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
